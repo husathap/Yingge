fe8c9387fdb42c7b611ea0fac1ee0cc9c72394b2
Sathaporn (Hubert) Hu <hubertth@hotmail.com>
2014-05-20 21:36:21 +0700

    Small changes.

--- Yingge/variableManifest.cpp
+++ Yingge/variableManifest.cpp
@@ -2,13 +2,17 @@
 
 #include "core/sessionSetup.h"
 
+// DO NOT TOUCH THIS PART! /////////////////////////////////////////////////////////////
 #define VARIABLES namespace yingge { void setVariables(yingge::Session *s) {
 #define STRING(name) s->strings.insert(std::pair<std::string, std::string>(#name, ""));
 #define NUMBER(name) s->numbers.insert(std::pair<std::string, double>(#name, 0));
 #define BOOLEAN(name) s->booleans.insert(std::pair<std::string, bool>(#name, false));
 #define xVARIABLES }}
+////////////////////////////////////////////////////////////////////////////////////////
 
 VARIABLES
-	STRING(Test)
-	NUMBER(Test)
+	BOOLEAN(DefeatedCrocobirdo)
+	BOOLEAN(DefeatedPyrite)
+	BOOLEAN(DefeatedVug)
+	BOOLEAN(DefeatedYOLOBear)
 xVARIABLES
